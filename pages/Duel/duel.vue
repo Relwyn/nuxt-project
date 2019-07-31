2547b4c32d2abdae1f0ce0a77560a7f4985cf44f
Tony Nguyen <tnguyen@activbrowser.com>
2019-07-31 18:07:39 +0200

    [feature] Start implementing Duel page

---
+++ pages/Duel/duel.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="duel">
+    <div class="field-container">
+      <div class="field-monster">
+        <div class="field"></div>
+        <div class="monster-zone"></div>
+        <div class="graveyard"></div>
+      </div>
+      <div class="field-spell">
+        <div class="extra-zone"></div>
+        <div class="spell-zone"></div>
+        <div class="main-deck"></div>
+      </div>
+    </div>
+  </section>
+</template>
