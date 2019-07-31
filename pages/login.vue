68c2de642d3d3f52bbcb35628f361424796d7a39
Tony Nguyen <tnguyen@activbrowser.com>
2019-07-31 17:28:05 +0200

    [feature] Create Login page

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="login">
+    <form @submit.prevent="login">
+      <input type="email" placeholder="Email" />
+      <input type="password" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {}
+  },
+  methods: {
+    login() {},
+    signin() {}
+  }
+}
+</script>
